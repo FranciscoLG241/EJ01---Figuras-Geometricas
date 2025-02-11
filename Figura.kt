a40c86daa18a6d14b3b87f843a5fc0ab7ea674bc
Fran <fletgas@g.educaand.es>
2025-02-11 19:55:10 +0100

    Modificacion del main y private radio

--- Figura.kt
+++ Figura.kt
@@ -13,7 +13,7 @@ abstract class Figura(val color: String) {
 
 
 
-class Circulo(color: String, val radio: Double): Figura(color){
+class Circulo(color: String, private val radio: Double): Figura(color){
 
     override fun area(): Double{
         return Math.PI * radio * radio
@@ -63,8 +63,13 @@ fun main() {
     val rectangulo = Rectangulo("Azul", 4.0, 6.0)
     val triangulo = Triangulo("Verde", 3.0, 4.0, 5.0, 6.0, 5.5)
 
+    println("\n **CÍRCULO**")
     println("Círculo - ${circulo.mostrarColor()}, Área: ${"%.2f".format(circulo.area())}, Perímetro: ${"%.2f".format(circulo.perimetro())}")
+
+    println("\n **RECTÁNGULO**")
     println("Rectángulo - ${rectangulo.mostrarColor()}, Área: ${rectangulo.area()}, Perímetro: ${rectangulo.perimetro()}")
+
+    println("\n **TRIÁNGULO**")
     println("Triángulo - ${triangulo.mostrarColor()}, Área: ${triangulo.area()}, Perímetro: ${triangulo.perimetro()}")
 }
 
